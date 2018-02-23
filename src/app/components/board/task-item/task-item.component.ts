5f7545e0fcdc2db351f4d3408efd502ae0938cd6
unknown <mazxaxz@gmail.com>
2018-02-23 16:50:44 +0100

    added ability to rename a task

--- src/app/components/board/task-item/task-item.component.ts
+++ src/app/components/board/task-item/task-item.component.ts
@@ -1,14 +1,40 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../../models/task';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html'
 })
-export class TaskItemComponent {
+export class TaskItemComponent implements OnInit {
   @Input('task') task: Task;
   @Output() update = new EventEmitter();
+
+  title = '';
+  editing = false;
+
   constructor() { }
+
+  ngOnInit() {
+    this.title = this.task.title;
+  }
+
+  toggleEdit() {
+    this.editing = !this.editing;
+  }
+
+  stopEditing() {
+    this.editing = false;
+  }
+
+  saveTask() {
+    if (this.editing) {
+      this.update.emit({
+        title: this.title.trim()
+      });
+
+      this.stopEditing();
+    }
+  }
 
   changeStatus() {
     this.update.emit({
