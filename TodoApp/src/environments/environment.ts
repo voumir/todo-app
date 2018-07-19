36edc06edc0a759e731a195fb98d57e1e2f1a8e3
Adrian <mazxaxz@gmail.com>
2018-07-19 10:53:14 +0200

    Separated Client side app

--- TodoApp/src/environments/environment.ts
+++ TodoApp/src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '<API_KEY>',
-    authDomain: '<PROJECT_ID>.firebaseapp.com',
-    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
-    projectId: '<PROJECT_ID>',
-    storageBucket: '<BUCKET>.appspot.com',
-    messagingSenderId: '<SENDER_ID>'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
