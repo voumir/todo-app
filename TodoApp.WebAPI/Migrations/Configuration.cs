2c0456444f8795accfb33d026693b9d3d55c7042
Adrian <mazxaxz@gmail.com>
2018-07-19 11:47:59 +0200

    Enabled migrations

---
+++ TodoApp.WebAPI/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TodoApp.WebAPI.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TodoApp.WebAPI.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TodoApp.WebAPI.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
