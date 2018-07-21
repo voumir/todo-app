4c1248db5f09046d845aeeae195aa0f8f5b11cfb
Adrian <mazxaxz@gmail.com>
2018-07-21 10:42:55 +0200

    Builded base mock for unit tests

---
+++ TodoApp.WebAPI.Tests/Controllers/AssignmentsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TodoApp.WebAPI.Controllers;
+using TodoApp.WebAPI.Core;
+using TodoApp.WebAPI.Core.Repositories;
+using TodoApp.WebAPI.Tests.Extensions;
+
+namespace TodoApp.WebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class AssignmentsControllerTests
+    {
+        private AssignmentsController _assignmentsController;
+        private Mock<IAssignmentsRepository> _mockRepository;
+        private string _userId;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockRepository = new Mock<IAssignmentsRepository>();
+
+            var mockUoW = new Mock<IUnitOfWork>();
+            mockUoW.SetupGet(u => u.Assignments).Returns(_mockRepository.Object);
+
+            _assignmentsController = new AssignmentsController(mockUoW.Object);
+            _userId = "1";
+            _assignmentsController.MockCurrentUser(_userId, "user1@domain.com");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TodoApp.WebAPI.Tests/Extensions/ApiExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Http;
+
+namespace TodoApp.WebAPI.Tests.Extensions
+{
+    public static class ApiExtensions
+    {
+        public static void MockCurrentUser(this ApiController controller, string userId, string userName)
+        {
+            var identity = new GenericIdentity(userName);
+            identity.AddClaim(
+                new Claim(
+                    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
+                    userName));
+
+            identity.AddClaim(
+                new Claim(
+                    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+                    userId));
+
+            var principal = new GenericPrincipal(identity, null);
+            controller.User = principal;
+        }
+    }
+}
